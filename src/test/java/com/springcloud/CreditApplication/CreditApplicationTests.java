9ebaca5027f3df0b499243fdeeaa8d22991e8daa
Seme'e <sam2020tek@gmail.com>
2023-06-19 15:28:39 -0400

    Task 1: initial commit

---
+++ src/test/java/com/springcloud/CreditApplication/CreditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloud.CreditApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
