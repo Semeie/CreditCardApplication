daa0d7ac8e98bbb75c86c5d11f43a25027df793e
Seme'e <sam2020tek@gmail.com>
2023-06-21 03:28:26 -0400

    Task 2: Added CreditCard entity

---
+++ src/main/java/com/springcloud/CreditApplication/entity/CreditCard.java
@@ -0,0 +1,31 @@
+package com.springcloud.CreditApplication.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "CREDIT_CARD")
+public class CreditCard{
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name= "FIRST_NAME")
+    private String firstName;
+    @Column(name = "LAST_NAME")
+    private String lastName;
+    @Column(name = "ANNUAL_INCOME")
+    private Integer annualIncome;
+    @Column(name = "ADDRESS")
+    private String address;
+    @Column(name = "isPublished")
+    private boolean publishStatus;
+    @Column(name = "REFERENCE_ID")
+    private String refId;
+
+}
