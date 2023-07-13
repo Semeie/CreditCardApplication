9f1304f3d02a02aa166987a17d360f9ef1a6a9a4
Seme'e <sam2020tek@gmail.com>
2023-07-12 23:42:17 -0400

    Task 12: Added CreditCardVerfication service

---
+++ CreditCardVerification/src/main/java/com/springcloud/CreditCardVerification/CreditCardVerificationApplication.java
@@ -0,0 +1,13 @@
+package com.springcloud.CreditCardVerification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardVerificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditCardVerificationApplication.class, args);
+	}
+
+}
---
+++ CreditCardVerification/src/test/java/com/springcloud/CreditCardVerification/CreditCardVerificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloud.CreditCardVerification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCardVerificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
