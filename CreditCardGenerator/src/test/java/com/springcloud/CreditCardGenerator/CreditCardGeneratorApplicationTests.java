9381ac9f6f24b98bdf59b410cffd59740b292ad6
Seme'e <sam2020tek@gmail.com>
2023-08-04 22:52:13 -0400

    Task 19: Added CreditCardGenerator service

---
+++ CreditCardGenerator/src/test/java/com/springcloud/CreditCardGenerator/CreditCardGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloud.CreditCardGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCardGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
