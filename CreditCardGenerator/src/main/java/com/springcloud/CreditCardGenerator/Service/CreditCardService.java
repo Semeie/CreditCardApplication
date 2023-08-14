b9e3f5a2367b313cb46b15425be84145154c1cdb
Seme'e <sam2020tek@gmail.com>
2023-08-13 22:57:03 -0400

    Task 23: Added CreditCardService class

--- CreditCardGenerator/src/main/java/com/springcloud/CreditCardGenerator/Service/CreditCardService.java
+++ CreditCardGenerator/src/main/java/com/springcloud/CreditCardGenerator/Service/CreditCardService.java
@@ -1,15 +1,53 @@
 package com.springcloud.CreditCardGenerator.Service;
 
+import com.springcloud.CreditCardGenerator.Entity.CreditCard;
 import com.springcloud.CreditCardGenerator.Event.CreditCardVerificationStatus;
 import com.springcloud.CreditCardGenerator.Repository.CreditCardRepo;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.math3.random.RandomDataGenerator;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+import java.util.stream.Collectors;
+@Service
+@AllArgsConstructor
+@Slf4j
 public class CreditCardService {
 
     private CreditCardRepo creditCardRepo;
 
     public void generateCreditCardNumberAndCvv
             (List<CreditCardVerificationStatus> creditCardVerificationStatus) {
+        var creditCards = creditCardVerificationStatus.stream().map(ccVerificationStatus-> {
+            var creditCard = new CreditCard();
+            BeanUtils.copyProperties(ccVerificationStatus, creditCard);
+            if(ccVerificationStatus.getStatus().equals
+                    (CreditCardVerificationStatus.VerificationStatus.APPROVED)){
+                log.info("**** Credit Card number will be generated as application is approved : {}",
+                        ccVerificationStatus.getRefId());
+                var creditCardNumber = generateRandomInt(100000000000L, 999999999999L);
+                creditCard.setCreditCardNumber(creditCardNumber);
+                var cvv = generateRandomInt(100L, 999L);
+                creditCard.setCvv(cvv);
+            }else{
+                log.info("**** Credit Card number will not be generated as application is rejected : {}",
+                        ccVerificationStatus.getRefId());
+                creditCard.setCreditCardNumber(0L);
+                creditCard.setCvv(0L);
+            }
+            creditCard.setStatus(ccVerificationStatus.getStatus().name());
+            return creditCard;
+        }).collect(Collectors.toList());
+
+        log.info("**** Saving Credit Cards ****");
+        creditCardRepo.saveAll(creditCards);
+        log.info("**** Saved Credit Cards ****");
+    }
+
+    private Long generateRandomInt(Long min, Long max) {
+        var randomDataGenerator = new RandomDataGenerator();
+        return randomDataGenerator.nextLong(min,max);
     }
 }
